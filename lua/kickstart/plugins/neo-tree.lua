50226c036678adad9c2f89c321311bb155974670
iternewo <215404099+iternewo@users.noreply.github.com>
2025-12-31 02:33:43 -0800

    neotree shenanigans with git integration leaderg

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,9 +12,27 @@ return {
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>g', ':Neotree git_status<CR>', desc = 'Neotree Git Status', silent = true },
   },
   opts = {
+    enable_git_status = true,
+    git_status = {
+      window = {
+        position = 'float',
+        mappings = {
+          ['A'] = 'git_add_all',
+          ['gu'] = 'git_unstage_file',
+          ['ga'] = 'git_add_file',
+          ['gr'] = 'git_revert_file',
+          ['gc'] = 'git_commit',
+          ['gp'] = 'git_push',
+          ['gg'] = 'git_commit_and_push',
+        },
+      },
+    },
+    close_if_last_window = true,
     filesystem = {
+      bind_to_cwd = true,
       window = {
         mappings = {
           ['\\'] = 'close_window',
