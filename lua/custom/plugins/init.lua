2504d9ddb5008a606aeda9e689c79af55d58bb5c
iternewo <215404099+iternewo@users.noreply.github.com>
2026-01-07 21:44:44 -0800

    hello lazygit in nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,6 +23,26 @@ return {
       { '<leader>gb', ':Neogit branch<CR>', desc = '[B]ranch', { silent = true, noremap = true } },
     },
   },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
   -- {
   --   'tpope/vim-fugitive',
   --   event = 'VimEnter',
