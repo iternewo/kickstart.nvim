7f8fbaf78f25786bf5f3cbaa473c88135e18614b
iternewo <215404099+iternewo@users.noreply.github.com>
2026-06-12 08:14:05 -0700

    last commit

--- init.lua
+++ init.lua
@@ -190,7 +190,6 @@ vim.api.nvim_create_user_command('Wq', 'wq', {})
 vim.api.nvim_create_user_command('WQ', 'wq', {})
 vim.api.nvim_create_user_command('Q', 'quit', {})
 
--- next and prev buffers aka homemade harpoon
 -- Buffer navigation in Normal mode using Lua API
 vim.keymap.set('n', '<leader>p', ':bprevious<CR>', { desc = '[P]revious buffer', silent = true })
 vim.keymap.set('n', '<leader>n', ':bnext<CR>', { desc = '[N]ext buffer', silent = true })
@@ -662,6 +661,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        ty = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -88,18 +88,18 @@ return {
       { '<leader>vs', '<cmd>LoveStop<cr>', ft = 'lua', desc = 'Stop LÖVE' },
     },
   },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {
-
-      anti_conceal = { enabled = false },
-    },
-  },
+  -- {
+  --   'MeanderingProgrammer/render-markdown.nvim',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  --   ---@module 'render-markdown'
+  --   ---@type render.md.UserConfig
+  --   opts = {
+  --
+  --     anti_conceal = { enabled = false },
+  --   },
+  -- },
   { 'mbbill/undotree' },
   {
     'ThePrimeagen/harpoon',
